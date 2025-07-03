150b4d41d85745684be73dcdad9a4884abcffeeb
HadyElMaageny <hadymaageny@gmail.com>
2025-07-03 16:29:12 +0400

    Day2-part2

--- src/app/Components/Order/ProductList/ProductList.component.ts
+++ src/app/Components/Order/ProductList/ProductList.component.ts
@@ -25,7 +25,7 @@ export class ProductListComponent implements OnInit {
         id: 1,
         name: 'Product 1',
         price: 100,
-        quantity: 10,
+        quantity: 1,
         imageUrl: 'https://picsum.photos/200/300',
         categoryID: 1
       },
@@ -41,7 +41,79 @@ export class ProductListComponent implements OnInit {
         id: 3,
         name: 'Product 3',
         price: 300,
-        quantity: 30,
+        quantity: 1,
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryID: 3
+      },
+      {
+        id: 4,
+        name: 'Product 4',
+        price: 400,
+        quantity: 0,
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryID: 1
+      },
+      {
+        id: 5,
+        name: 'Product 5',
+        price: 500,
+        quantity: 1,
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryID: 2
+      },
+      {
+        id: 6,
+        name: 'Product 6',
+        price: 600,
+        quantity: 60,
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryID: 3
+      },
+      {
+        id: 7,
+        name: 'Product 7',
+        price: 700,
+        quantity: 70,
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryID: 1
+      },
+      {
+        id: 8,
+        name: 'Product 8',
+        price: 800,
+        quantity: 80,
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryID: 2
+      },
+      {
+        id: 9,
+        name: 'Product 9',
+        price: 900,
+        quantity: 90,
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryID: 3
+      },
+      {
+        id: 10,
+        name: 'Product 10',
+        price: 1000,
+        quantity: 0,
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryID: 1
+      },
+      {
+        id: 11,
+        name: 'Product 11',
+        price: 1100,
+        quantity: 110,
+        imageUrl: 'https://picsum.photos/200/300',
+        categoryID: 2
+      },
+      {
+        id: 12,
+        name: 'Product 12',
+        price: 1200,
+        quantity: 0,
         imageUrl: 'https://picsum.photos/200/300',
         categoryID: 3
       }
@@ -53,6 +125,10 @@ export class ProductListComponent implements OnInit {
     if (product) {
       product.quantity -= Number(itemsCount);
     }
+  }
+
+  prdTrackByfn(index: number, item: IProduct): number {
+    return item.id;
   }
 
   ngOnInit() {
