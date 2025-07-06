8e53325518c5c804864edd65562beceee84be118
HadyElMaageny <87091270+HadyElMaageny@users.noreply.github.com>
2025-07-07 00:13:03 +0400

    Day3-part5

--- src/app/Components/Order/order-master/order-master.component.ts
+++ src/app/Components/Order/order-master/order-master.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit} from '@angular/core';
 import { ICategory } from '../../../Models/ICategory';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -10,10 +10,14 @@ import { ProductListComponent } from "../ProductList/ProductList.component";
   styleUrls: ['./order-master.component.css'],
   imports: [CommonModule, FormsModule, ProductListComponent]
 })
-export class OrderMasterComponent implements OnInit {
+
+
+export class OrderMasterComponent implements OnInit, AfterViewInit {
   catList: ICategory[];
   selectedCatID: number = 0;
   recievedOrderTotalPrice: number = 0;
+  @ViewChild('customerNameInput') clientNameInputElement!: ElementRef;
+  @ViewChild(ProductListComponent) productListComponent!: ProductListComponent;
   constructor() {
     this.catList = [
       { id: 1, name: 'Category 1' },
@@ -21,12 +25,18 @@ export class OrderMasterComponent implements OnInit {
       { id: 3, name: 'Category 3' }
     ];
   }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
   onTotalPriceChanged(orderTotalPrice: number) {
     this.recievedOrderTotalPrice = orderTotalPrice;
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.clientNameInputElement.nativeElement.value = 'Default Client Name';
+    this.clientNameInputElement.nativeElement.style = 'background-color: yellow; color: red; font-size: 20px; font-weight: bold;';
+    console.log(this.productListComponent.prdList);
   }
 
 }
